b50067a459132e6046c662adea096a49e61419ea
Evgeny Shevelev <jshvlv@list.ru>
2020-12-06 17:49:33 +0300

    Adding properties to recources

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -3,12 +3,36 @@ package org.example;
 public class MusicPlayer {
     private Music music;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private String name;
+    private int volume;
+
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+    public MusicPlayer(){}
     //This is IoC
     public  MusicPlayer(Music music){
         this.music = music;
     }
 
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + music.getSong() + " \nPlayer name: " + this.getName() + ", volume: " + this.getVolume());
     }
 }
